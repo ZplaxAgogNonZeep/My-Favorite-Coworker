8c649054144a563a6f5647e2da351146c682ba84
Aibarret <83474314+ZplaxAgogNonZeep@users.noreply.github.com>
2023-10-17 17:26:42 -0400

    Messing around with UI Themes

--- Digital Virtual Pets/Device/Background.gd
+++ Digital Virtual Pets/Device/Background.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+enum MenuState {MINIMIZED, FEED, PLAY, STATS}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +9,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
+
+func _unhandled_input(event):
 	pass

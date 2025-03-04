6a9f2caa17ebb7a83285bdf618ad2ef0086cdebc
ZplaxAgogNonZeep <83474314+ZplaxAgogNonZeep@users.noreply.github.com>
2025-03-04 16:54:02 -0500

    Created a tool that will parse a JSON from twine.
    needs to be finished and tested still but generally it uses twine tags to make a list of characters and their available conversations

---
+++ Digital Virtual Pets/data/scripts/dialog_data.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name CharacterDialog
+
+@export var characterName : String
+@export var conversations : Array[Dictionary]
+
+var passages : Dictionary
---
+++ Digital Virtual Pets/do_not_build/tools/dialog_writer/dialog_writer.gd
@@ -0,0 +1,68 @@
+@tool
+extends Node
+#TODO: Document
+@export_tool_button("Generate Dialog Resources")
+var x: Callable = _generateDialog
+@export var dialogText : JSON
+
+var _passageList : Array
+
+func _generateDialog():
+	print("Generating dialog from JSON file")
+	
+	#var test = CharacterDialog.new()
+	#test.doot = "meeple"
+	#ResourceSaver.save(test, "res://test_dialog.tres")
+	
+	var dataReceived = dialogText.data
+	
+	if (typeof(dataReceived) == TYPE_DICTIONARY):
+		if (typeof(dataReceived["passages"]) == TYPE_ARRAY):
+			_createDialogFiles(dataReceived["passages"])
+		else:
+			print("DIALOG WRITER ERROR: Unexpected Format in Passages, please re-export file and try again.")
+	else:
+		print("DIALOG WRITER ERROR: Unexpected Format, please re-export file and try again.")
+
+func _createDialogFiles(rawDialog : Array):
+	print("ARRAY")
+	_passageList = rawDialog.duplicate(true)
+	var characterList : Array[Array] = []
+	var characterConversations : Dictionary[String, Array]
+	
+	# Creats a list of characters to make resources for
+	# characters are determined by the first tag in a passage
+	for passage : Dictionary in rawDialog:
+		if (passage["tags"] != ""):
+			var tags : Array = passage["tags"].split(" ", false)
+			
+			if (tags.size() > 1):
+				if (tags[0] == "Conversation-Start"):
+					if (characterConversations.has(tags[1])):
+						characterConversations[tags[1]].append(passage)
+					else:
+						characterConversations[tags[1]] = [passage]
+	
+	for characterName in characterConversations.keys():
+		var characterDialog : CharacterDialog = CharacterDialog.new()
+		
+		characterDialog.characterName = characterName
+		characterDialog.conversations = characterConversations[characterName].duplicate(true)
+		# Holy SHIT this one was complicated. i HATE recursion
+		for conversation : Dictionary in characterDialog.conversations:
+			_getAllLinkedPassages(conversation, characterDialog.passages)
+
+
+func _getAllLinkedPassages(passage : Dictionary, linkedPassages):
+	if (passage.has("links")):
+		for link : Dictionary in passage["links"]:
+			var linkedPassage = _getPassageByName(link["passageName"])
+			linkedPassages.append(linkedPassage)
+			_getAllLinkedPassages(linkedPassage, linkedPassages)
+
+
+func _getPassageByName(passageName : String) -> Dictionary:
+	for passage : Dictionary in _passageList:
+		if (passage["name"] == passageName):
+			return passage
+	return {}

f47a64dcad4b06191ad6cce63f2a91f625a1cfc5
ZplaxAgogNonZeep <83474314+ZplaxAgogNonZeep@users.noreply.github.com>
2025-03-20 11:57:59 -0400

    Reworked Stat Menu
    Set it up kinda like older tamagotchi/digimon menus. Should work fine. will probably do the same for the minigames

--- Digital Virtual Pets/scene_manager.gd
+++ Digital Virtual Pets/scene_manager.gd
@@ -32,15 +32,15 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Debug2"):
 		pass
 	
-	if (event is InputEventMouseButton):
-		if (event.double_click and _menuManager.isMenuOpen() and _iconMenu == _menuManager.getActiveMenu()):
-			_menuManager.toggleMenu(false)
-			get_tree().create_tween().tween_property(_background, "modulate", Color(Color.WHITE, 0.0), .75)
-			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
-		elif (event.double_click and not _menuManager.isMenuOpen()):
-			_menuManager.toggleMenu(true)
-			get_tree().create_tween().tween_property(_background, "modulate", Color(Color.WHITE, 1), .75)
-			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+	#if (event is InputEventMouseButton):
+		#if (event.double_click and _menuManager.isMenuOpen() and _iconMenu == _menuManager.getActiveMenu()):
+			#_menuManager.toggleMenu(false)
+			#get_tree().create_tween().tween_property(_background, "modulate", Color(Color.WHITE, 0.0), .75)
+			#DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+		#elif (event.double_click and not _menuManager.isMenuOpen()):
+			#_menuManager.toggleMenu(true)
+			#get_tree().create_tween().tween_property(_background, "modulate", Color(Color.WHITE, 1), .75)
+			#DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
 
 
 func _displayDevice(skipAnimation := false):
--- Digital Virtual Pets/ui/menu_buttons/stat_menu.gd
+++ Digital Virtual Pets/ui/menu_buttons/stat_menu.gd
@@ -2,23 +2,25 @@ extends Node2D
 
 var implements = [Interface.MenuState]
 
+@export var _panelList : Array[Node2D]
 var stateMachine : Node2D
+var _index : int
 
 func initializeMenu():
 		visible = true
 		GameEvents.PauseGame.emit()
 		
 		if (get_tree().get_nodes_in_group("Pet").size() > 0):
-			var pet = get_tree().get_nodes_in_group("Pet")[0]
-			$Personality.text = "Personality: " + Enums.Personality.keys()[pet.personality]
-			$Name.text = pet.petResource.name
-			$Pow/Value.text = str(pet.abilityStats[Enums.AbilityStat.POW])
-			$End/Value.text = str(pet.abilityStats[Enums.AbilityStat.END])
-			$Spd/Value.text = str(pet.abilityStats[Enums.AbilityStat.SPD])
-			$Bal/Value.text = str(pet.abilityStats[Enums.AbilityStat.BAL])
+			var pet = PetManager.instance.activePet
+			$StatPanel/Personality.text = "Personality:\n" + Enums.Personality.keys()[pet.personality]
+			$DisplayPanel/Name.text = pet.petResource.name
+			$StatPanel/Pow/Value.text = str(pet.abilityStats[Enums.AbilityStat.POW])
+			$StatPanel/End/Value.text = str(pet.abilityStats[Enums.AbilityStat.END])
+			$StatPanel/Spd/Value.text = str(pet.abilityStats[Enums.AbilityStat.SPD])
+			$StatPanel/Bal/Value.text = str(pet.abilityStats[Enums.AbilityStat.BAL])
 			
-			$"Display Sprite".texture = pet.getSpriteIcon()
-			$"Display Sprite".offset = pet.sprite.offset
+			$"DisplayPanel/Display Sprite".texture = pet.getSpriteIcon()
+			$"DisplayPanel/Display Sprite".offset = pet.sprite.offset
 			
 			var count = 0
 			for i in pet.evolvedFromIcons:
@@ -33,17 +35,35 @@ func exitMenu():
 	print("Exit Menu called")
 	visible = false
 	
-	$Personality.text = "Personality: ??????"
+	$StatPanel/Personality.text = "Personality: ??????"
 	
-	$Pow/Value.text = "00"
-	$End/Value.text = "00"
-	$Spd/Value.text = "00"
-	$Bal/Value.text = "00"
+	$StatPanel/Pow/Value.text = "0"
+	$StatPanel/End/Value.text = "0"
+	$StatPanel/Spd/Value.text = "0"
+	$StatPanel/Bal/Value.text = "0"
+	_index = 0
 	
 	GameEvents.UnpauseGame.emit()
 
 
 func takeInput(input : Enums.DeviceButton):
+	if (input == Enums.DeviceButton.RIGHT_BUTTON):
+		_panelList[_index].visible = false
+		_index += 1
+		if (_index >= _panelList.size()):
+			_index = 0
+		if (_index < 0):
+			_index = _panelList.size() - 1
+		_panelList[_index].visible = true
+	if (input == Enums.DeviceButton.LEFT_BUTTON):
+		_panelList[_index].visible = false
+		_index -= 1
+		if (_index >= _panelList.size()):
+			_index = 0
+		if (_index < 0):
+			_index = _panelList.size() - 1
+		_panelList[_index].visible = true
+	
 	if (input == Enums.DeviceButton.CENTER_BUTTON):
 		closeMenu()
 

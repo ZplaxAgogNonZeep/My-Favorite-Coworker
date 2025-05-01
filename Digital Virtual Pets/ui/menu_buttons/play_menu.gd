042c22f6961443f7512c3fc72805224c0d92d645
ZplaxAgogNonZeep <83474314+ZplaxAgogNonZeep@users.noreply.github.com>
2025-05-01 15:35:30 -0400

    Minigame menu updated
    it's now a scrolling menu, seems to work pretty well. now we just need to update all of the minigames

--- Digital Virtual Pets/ui/menu_buttons/play_menu.gd
+++ Digital Virtual Pets/ui/menu_buttons/play_menu.gd
@@ -3,34 +3,37 @@ extends Node2D
 var implements = [Interface.MenuState]
 enum PlayState {MENU, GAME}
 
-@export var buttonController : Node
+@export var _menuIndexContainer : Node2D
+@export var _gamePageList : Array[Node2D]
 @export var miniGameList : Array[PackedScene]
 
 var stateMachine : Node2D
 var game : Control
 var state = PlayState.MENU
+var _menuIndex := 0
 
 func _ready():
 	pass
-	#$GameMenu/Exit.ButtonSelected.connect(onExitSelected)
-	#$GameMenu/Guess.ButtonSelected.connect(onGuessSelected)
-	#$GameMenu/Fish.ButtonSelected.connect(onFishSelected)
-	#$GameMenu/Run.ButtonSelected.connect(onRunSelected)
-	#$GameMenu/Hit.ButtonSelected.connect(onHitSelected)
-	#$GameMenu/Endure.ButtonSelected.connect(onEndureSelected)
 
 
 func initializeMenu():
 	state = PlayState.MENU
 	visible = true
 	GameEvents.PauseGame.emit()
-	buttonController.setActive(true)
+	#buttonController.setActive(true)
+	for page in _gamePageList:
+		page.visible = false
+	_gamePageList[0].visible = true
+	_menuIndex = 0
+	for x in range(_menuIndexContainer.get_child_count()):
+		_setVisualMenuIndex(x, false)
+	_setVisualMenuIndex(0, true)
 
 
 func exitMenu():
 	visible = false
 	game = null
-	buttonController.setActive(false)
+	#buttonController.setActive(false)
 	GameEvents.HopDeviceOnce.emit()
 	GameEvents.HopDeviceOnce.emit()
 	GameEvents.UnpauseGame.emit()
@@ -43,20 +46,53 @@ func takeInput(input : Enums.DeviceButton):
 		PlayState.MENU:
 			match input:
 				Enums.DeviceButton.CENTER_BUTTON:
-					buttonController.select()
+					_select(_menuIndex)
 				Enums.DeviceButton.LEFT_BUTTON:
-					buttonController.cycle(-1)
+					_cycle(-1)
 				Enums.DeviceButton.RIGHT_BUTTON:
-					buttonController.cycle(1)
+					_cycle(1)
 		PlayState.GAME:
 			if game:
 				game.takeInput(input)
 
-func getPet():
-	if (get_tree().get_nodes_in_group("Pet").size() > 0):
-		return get_tree().get_nodes_in_group("Pet")[0]
+
+func getPet() -> Pet:
+	if (PetManager.instance != null):
+		return PetManager.instance.activePet
 	else:
 		return null
+
+
+func _setVisualMenuIndex(index : int, active : bool):
+	_menuIndexContainer.get_node("Index" + str(index)).frame = active
+	
+
+func _cycle(amount : int):
+	_setVisualMenuIndex(_menuIndex, false)
+	_gamePageList[_menuIndex].visible = false
+	_menuIndex += amount
+	
+	if (_menuIndex >= _gamePageList.size()):
+		_menuIndex = 0
+	if (_menuIndex < 0):
+		_menuIndex = _gamePageList.size() - 1
+	
+	_setVisualMenuIndex(_menuIndex, true)
+	_gamePageList[_menuIndex].visible = true
+
+
+func _select(index : int):
+	if (index == _gamePageList.size() - 1):
+		onExitSelected()
+		return
+	
+	if getPet():
+		game = miniGameList[_menuIndex].instantiate()
+		$MiniGameContainer.add_child(game)
+		game.startGame(getPet(), self)
+		state = PlayState.GAME
+	else:
+		print("COULD NOT FIND PET")
 
 func onExitSelected():
 	closeMenu()

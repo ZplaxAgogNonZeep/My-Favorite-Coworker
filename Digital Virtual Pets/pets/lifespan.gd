7777f5341d7ac315e0d7c39bd928d7be6eee2593
ZplaxAgogNonZeep <83474314+ZplaxAgogNonZeep@users.noreply.github.com>
2025-06-11 11:50:09 -0400

    Fixed Lifespan Timer
    Need to keep in mind, old save data from versions before 0.3.1 will not be compatible and will display very strange ages

--- Digital Virtual Pets/pets/lifespan.gd
+++ Digital Virtual Pets/pets/lifespan.gd
@@ -2,45 +2,50 @@ extends Node
 
 class_name Lifespan
 
-var _lifespan := 0.0
+var _lifespan : int = 0
+var _secondsTimer := 0.0
 var _paused := false
 
 static func convertLifespanToAge(lifespan : float) -> Array:
 	var rawAge = lifespan
 	var formattedAge = [0,0,0]
 	
-	if (rawAge > 86400):
+	if (rawAge > 1440):
 		# age is bigger than one day
-		var convertedAge = rawAge / 86400
-		formattedAge[0] = floorf(convertedAge)
-		formattedAge[1] = rawAge - (86400 * formattedAge[0])
-	if (rawAge > 3600):
+		var convertedAge = rawAge / 1440
+		formattedAge[0] = floori(convertedAge)
+		formattedAge[1] = rawAge - (1440 * formattedAge[0])
+	if (rawAge > 60):
 		# age is bigger than one hour
-		if (formattedAge[0] > 0 and formattedAge[1] > 3600):
-			var convertedAge = formattedAge[1] / 3600
-			formattedAge[2] = formattedAge[1] - (3600 * floorf(convertedAge))
-			formattedAge[1] = floorf(convertedAge)
+		if (formattedAge[0] > 0 and formattedAge[1] > 60):
+			var convertedAge = formattedAge[1] / 60
+			formattedAge[2] = formattedAge[1] - (60 * floori(convertedAge))
+			formattedAge[1] = floori(convertedAge)
 		elif (formattedAge[0] > 0 ):
 			formattedAge[2] = formattedAge[1]
 			formattedAge[1] = 0
 		else:
-			var convertedAge = rawAge / 3600
-			formattedAge[1] = floorf(convertedAge)
-			formattedAge[2] = formattedAge[1] - (3600 * formattedAge[1])
-	if (rawAge > 60):
+			var convertedAge = rawAge / 60
+			formattedAge[1] = floori(convertedAge)
+			formattedAge[2] = formattedAge[1] - (60 * formattedAge[1])
+	if (rawAge > 0):
 		# age is bigger than one minute
 		if (formattedAge[2] > 0):
-			var convertedAge = formattedAge[2] / 60
-			formattedAge[2] = floorf(convertedAge)
+			var convertedAge = formattedAge[2]
+			formattedAge[2] = floori(convertedAge)
 		else:
-			var convertedAge = rawAge / 60
+			var convertedAge = rawAge
 			formattedAge[2] = floorf(convertedAge)
 	
 	return formattedAge
 
 func _process(delta: float) -> void:
 	if (!_paused):
-		_lifespan += delta
+		if (_secondsTimer <= 60):
+			_secondsTimer += delta
+		else:
+			_secondsTimer = 0
+			_lifespan += 1
 
 
 func getLifespan() -> float:

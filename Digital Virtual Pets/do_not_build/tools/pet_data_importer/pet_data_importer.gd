5b38f106272ee62da11b3289202a24983f3edd02
Zplax <zplaxstreamssometimes@gmail.com>
2025-09-16 13:50:48 -0400

    Jettisoning corrupted pet data
    really hoping this is the problem with the pet data importer

--- Digital Virtual Pets/do_not_build/tools/pet_data_importer/pet_data_importer.gd
+++ Digital Virtual Pets/do_not_build/tools/pet_data_importer/pet_data_importer.gd
@@ -12,6 +12,8 @@ func _fillPetData():
 	var file = FileAccess.open(_rootFilepath, FileAccess.READ)
 	var lines = file.get_as_text().replace("\r", "").split("\n")
 	for line in lines:
+		if (line == ""):
+			continue
 		var petDataList : PackedStringArray = line.split("\t")
 		var petData = _getResourceFromName(petDataList[0], petDataList[1])
 		var writeToFile = false
@@ -20,27 +22,32 @@ func _fillPetData():
 			print("Found no matching data for ", petDataList[0])
 			petData = PetTypeData.new()
 			petData.spriteFrames = _placeholderSpriteFrames
-			ResourceSaver.save(petData, _petDataFilePathRoot + "stage_" + str(petData.stage) + "/" + petData.name.to_lower() + ".tres")
+			petData.name = petDataList[0]
+			ResourceSaver.save(petData, _petDataFilePathRoot + "stage_" + petDataList[1] + "/" + petDataList[0].to_lower() + ".tres")
 			petData = _getResourceFromName(petDataList[0], petDataList[1])
 		
 		petData.name = petDataList[0]
 		print("Updateing ", petData.name)
 		petData.stage = int(petDataList[1])
 		petData.encyclopediaEntry = petDataList[2]
-		petData.evolutionConditions.clear()
+		
+		# This whole next part is entirely built around setting up evolution conditions,
+		# we'll do this section twice in order to account for parallel evolutions.
+		#region Normal Evo Conditions
 		# Okay so indexes 4-13 is for evolution conditions, in that, there's some extra formatting
 		# that needs to be done to enter that: [condition group number]-AND/OR+Stat Value
 		# to parse this, I'm going to create a dict of arrays that will contain lines to be parsed
 		# each group representing one condition with the first element being the "default" one
+		petData.evolutionConditions.clear()
 		var groups : Dictionary[String, Array]
 		groups[""] = []
 		
 		var count = 4
-		while count <= 9:
+		while count <= 13:
 			if (petDataList[count] == "" or petDataList[count] == "N/A"):
 				count += 1
 				continue
-			
+			print(petDataList[0])
 			var statString
 			match count:
 				4:
@@ -138,19 +145,143 @@ func _fillPetData():
 		
 		for condition in andConditions:
 			petData.evolutionConditions.append(condition)
+		#endregion
+		
+		#region Parallel Conditions
+		# repeating the ENTIRE process to account for parallel Evolution Conditions	14-23
+		petData.parallelConditions.clear()
+		groups = {}
+		groups[""] = []
+		
+		count = 14
+		while count <= 23:
+			if (petDataList[count] == "" or petDataList[count] == "N/A"):
+				count += 1
+				continue
 			
+			var statString
+			match count:
+				14:
+					statString = "POW|"
+				15:
+					statString = "END|"
+				16:
+					statString = "SPD|"
+				17:
+					statString = "BAL|"
+				18:
+					statString = "STATTOTAL|"
+				19:
+					statString = "TRAUMA|"
+				20:
+					statString = "CONDITION1|"
+				21:
+					statString = "CONDITION2|"
+				22:
+					statString = "CONDITION3|"
+				23:
+					statString = "CONDITION4|"
+					
+			if petDataList[count].contains("+"):
+				if groups.has(petDataList[count].split("+")[0]):
+					groups[petDataList[count].split("+")[0]].append(statString + petDataList[count].split("+")[1])
+				else:
+					groups[petDataList[count].split("+")[0]] = [statString + petDataList[count].split("+")[1]]
+			else:
+				groups[""].append(statString + petDataList[count])
+			
+			count += 1
+		
+		andConditions = []
+		for groupKey in groups.keys():
+			if (groups[groupKey].size() == 0):
+				continue
+				
+			var condition = EvolutionCondition.new()
+			
+			if groupKey == "":
+				condition.conditionLogic = EvolutionCondition.LogicalConditionals.AND
+			else:
+				match groupKey.split("-")[1]:
+					"AND":
+						condition.conditionLogic = EvolutionCondition.LogicalConditionals.AND
+					"OR":
+						condition.conditionLogic = EvolutionCondition.LogicalConditionals.OR
+			
+			for stat : String in groups[groupKey]:
+				match stat.split("|")[0]:
+					"POW":
+						condition.POW = int(stat.split("|")[1])
+					"END":
+						condition.END = int(stat.split("|")[1])
+					"SPD":
+						condition.SPD = int(stat.split("|")[1])
+					"BAL":
+						condition.BAL = int(stat.split("|")[1])
+					"STATTOTAL":
+						condition.statTotal = int(stat.split("|")[1])
+					"TRAUMA":
+						match stat.split("|")[1][0]:
+							'>':
+								condition.TraumaGreater = stat.split("|")[1].substr(1)
+							'<':
+								condition.TraumaLesser = stat.split("|")[1].substr(1)
+							'=':
+								condition.TraumaEqual = stat.split("|")[1].substr(1)
+					"CONDITION1":
+						match stat.split("|")[1].to_upper():
+							"NONE":
+								condition.conditions.append(EvolutionCondition.StatusCondition.NONE)
+							"ANY CONDITION":
+								condition.conditions.append(EvolutionCondition.StatusCondition.ANY)
+							"OVERFED":
+								condition.conditions.append(EvolutionCondition.StatusCondition.OVERFED)
+							"OVERSTIMULATED":
+								condition.conditions.append(EvolutionCondition.StatusCondition.OVERSTIMULTED)
+							"HUNGRY":
+								condition.conditions.append(EvolutionCondition.StatusCondition.HUNGRY)
+							"BORED":
+								condition.conditions.append(EvolutionCondition.StatusCondition.BORED)
+							"STINKY":
+								condition.conditions.append(EvolutionCondition.StatusCondition.STINKY)
+							"ANXIOUS":
+								condition.conditions.append(EvolutionCondition.StatusCondition.ANXIOUS)
+							_:
+								print("PARSE ERROR: Could not parse Status Condition: ", stat.split("|")[1].to_upper())
+			
+			if (condition.conditionLogic == EvolutionCondition.LogicalConditionals.AND):
+				andConditions.append(condition)
+			else:
+				petData.parallelConditions.append(condition)
+		
+		for condition in andConditions:
+			petData.parallelConditions.append(condition)
 	
+	#endregion
 	# Now finally we do evolutions now that we've ensured that all pets from list
-	# have been added
+	# have been added, to account for parallel evolutions we'll also need to seperate "|"
 	for line in lines:
+		if line == "":
+			continue
 		var petDataList : PackedStringArray = line.split("\t")
 		var petData = _getResourceFromName(petDataList[0], petDataList[1])
 		petData.evolutions.clear()
+		petData.parallelEvolutions.clear()
 		
 		if petDataList[3] != "":
-			for evolutionName in petDataList[3].split(","):
+			var evolutionString = petDataList[3]
+			var parallelString = ""
+			if (petDataList[3].contains("|")):
+				evolutionString = petDataList[3].split("|")[0]
+				parallelString = petDataList[3].split("|")[1]
+			
+			for evolutionName in evolutionString.split(","):
 				print("evo: ",_getResourceFromName(evolutionName, str(petData.stage + 1)))
 				petData.evolutions.append(_getResourceFromName(evolutionName, str(petData.stage + 1)))
+			if parallelString != "":
+				for evolutionName in parallelString.split(","):
+					print("parallel evo: ",_getResourceFromName(evolutionName, str(petData.stage + 1)))
+					petData.parallelEvolutions.append(_getResourceFromName(evolutionName, str(petData.stage + 1)))
 		ResourceSaver.save(petData, _petDataFilePathRoot + "stage_" + str(petData.stage) + "/" + _getResourceFileNameFromName(petData.name, str(petData.stage)))
 	
 	
